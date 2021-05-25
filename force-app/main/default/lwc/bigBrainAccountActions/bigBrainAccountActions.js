8fd9992e4187168f5566f742fa153fd31d96bedd
eranbenmoha <eranb@monday.com>
2021-05-25 14:28:58 +0300

    f

--- force-app/main/default/lwc/bigBrainAccountActions/bigBrainAccountActions.js
+++ force-app/main/default/lwc/bigBrainAccountActions/bigBrainAccountActions.js
@@ -1,9 +1,17 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import { getRecord, getFieldValue, getFieldDisplayValue } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import setAccountFreeUsers from '@salesforce/apex/BigBrainController.setAccountFreeUsers';
 
 import ACCOUNT_FIELD from '@salesforce/schema/Lead.primary_pulse_account_id__c';
-
 const fields = [ACCOUNT_FIELD];
+
+const formatDate = (date) => {
+  var dd = String(date.getDate()).padStart(2, '0');
+  var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = date.getFullYear();
+  return mm + '/' + dd + '/' + yyyy;
+} 
 
 export default class BigBrainAccountActions extends LightningElement {
   @api recordId;
@@ -15,25 +23,46 @@ export default class BigBrainAccountActions extends LightningElement {
     return getFieldValue(this.lead.data, ACCOUNT_FIELD);
   }
 
+  get oneMonthFromNow(){
+    const today = new Date();
+    today.setMonth(today.getMonth() + 1);
+    return formatDate(today);
+  }
+
+  get todaysDate() {
+    const today = new Date();
+    return formatDate(today);
+  }
+
   freeUsersAmount = 0;
-  freeUsersUntil = null;
+  freeUsersUntil = "";
 
   handleFreeUsersAmountChange(e) {
-    console.log(e.detail.value);
-    console.log(e.target.value);
     this.freeUsersAmount = e.detail.value;
   }
 
   handleFreeUsersUntilChange(e) {
-    console.log(e.detail.value);
-    console.log(e.target.value);
     this.freeUsersUntil = e.detail.value;
 }
 
   handleFreeUsersGrantClick(e) {
-    alert(this.accountId);
-    console.log(this.freeUsersAmount);
-    console.log(this.freeUsersUntil);
-    console.log(this.accountId);
+    setAccountFreeUsers({pulseAccountId: this.accountId, freeUsers: this.freeUsersAmount, untilDate: this.freeUsersUntil})
+      .then(result => {
+        const evt = new ShowToastEvent({
+          title: "Set free users successfully!",
+          message: 'Free users was set to ' + this.freeUsersAmount,
+          variant: "success",
+        });
+
+        this.dispatchEvent(evt);
+      })
+      .catch(error => {
+        const evt = new ShowToastEvent({
+          title: "Error while setting free users",
+          variant: "error",
+        });
+
+        this.dispatchEvent(evt);
+      });
   }
 }
