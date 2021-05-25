b518f569bebc1bef56375bcd96fa9987eda84e3e
eranbenmoha <eranb@monday.com>
2021-05-25 16:19:58 +0300

    pickers

--- force-app/main/default/lwc/expectedPlanPicker/expectedPlanPicker.js
+++ force-app/main/default/lwc/expectedPlanPicker/expectedPlanPicker.js
@@ -1,3 +1,36 @@
 import { LightningElement } from 'lwc';
 
-export default class ExpectedPlanPicker extends LightningElement {}
+export default class ExpectedPlanPicker extends LightningElement {
+  currency = null;
+  pricingVersion = null;
+
+  tier = null;
+  period = null;
+  users = null;
+  pps = null;
+  discount = null;
+
+  get tierOptions() {
+    return [
+        { label: 'Basic', value: 'basic' },
+        { label: 'Standard', value: 'standard' },
+        { label: 'Pro', value: 'pro' },
+        { label: 'Enterprise', value: 'enterprise' },
+    ];
+  }
+
+  handleTierChange(event) {
+    this.tier = event.detail.value;
+  }
+
+  get periodOptions() {
+    return [
+        { label: 'Monthly', value: 'monthly' },
+        { label: 'Yearly', value: 'yearly' },
+    ];
+  }
+
+  handlePeriodChange(event) {
+    this.period = event.detail.value;
+  }
+}
