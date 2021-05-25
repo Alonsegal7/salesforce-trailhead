8d98b16ee7f305fbc74ddb4ebf187f340e2b38b2
eranbenmoha <eranb@monday.com>
2021-05-25 10:52:41 +0300

    c

--- force-app/main/default/lwc/bigBrainAccountActions/bigBrainAccountActions.js
+++ force-app/main/default/lwc/bigBrainAccountActions/bigBrainAccountActions.js
@@ -1,3 +1,39 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
+import { getRecord, getFieldValue, getFieldDisplayValue } from 'lightning/uiRecordApi';
 
-export default class BigBrainAccountActions extends LightningElement {}
+import ACCOUNT_FIELD from '@salesforce/schema/Lead.primary_pulse_account_id__c';
+
+const fields = [ACCOUNT_FIELD];
+
+export default class BigBrainAccountActions extends LightningElement {
+  @api recordId;
+
+  @wire(getRecord, { recordId: '$recordId', fields })
+  lead;
+
+  get accountId() {
+    return getFieldValue(this.lead.data, ACCOUNT_FIELD);
+  }
+
+  freeUsersAmount = 0;
+  freeUsersUntil = null;
+
+  handleFreeUsersAmountChange(e) {
+    console.log(e.detail.value);
+    console.log(e.target.value);
+    this.freeUsersAmount = e.detail.value;
+  }
+
+  handleFreeUsersUntilChange(e) {
+    console.log(e.detail.value);
+    console.log(e.target.value);
+    this.freeUsersUntil = e.detail.value;
+}
+
+  handleFreeUsersGrantClick(e) {
+    alert(this.accountId);
+    console.log(this.freeUsersAmount);
+    console.log(this.freeUsersUntil);
+    console.log(this.accountId);
+  }
+}
