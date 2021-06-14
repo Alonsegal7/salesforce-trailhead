6935c19a732e7bad43668f7f0c9a54546ecead07
eranbenmoha <eranb@monday.com>
2021-06-14 18:23:33 +0300

    works on opp

--- force-app/main/default/lwc/bigBrainAccountTabs/bigBrainAccountTabs.js
+++ force-app/main/default/lwc/bigBrainAccountTabs/bigBrainAccountTabs.js
@@ -1,8 +1,9 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
-import ACCOUNT_FIELD from '@salesforce/schema/Lead.primary_pulse_account_id__c';
+import LEAD_ACCOUNT_FIELD from '@salesforce/schema/Lead.primary_pulse_account_id__c';
+import OPP_ACCOUNT_FIELD from "@salesforce/schema/Opportunity.primary_pulse_account_id__c";
 
-const fields = [ACCOUNT_FIELD];
+const fields = [LEAD_ACCOUNT_FIELD, OPP_ACCOUNT_FIELD];
 
 export default class BigBrainAccountTabs extends LightningElement {
     pulseAccountId; 
@@ -11,7 +12,7 @@ export default class BigBrainAccountTabs extends LightningElement {
     @wire(getRecord, { recordId: '$recordId', fields })
     wiredRecord({ error, data }) {
         if (data) {
-            this.pulseAccountId = getFieldValue(data, ACCOUNT_FIELD)
+            this.pulseAccountId = getFieldValue(data, LEAD_ACCOUNT_FIELD) || getFieldValue(data, OPP_ACCOUNT_FIELD)
         }
     }
 
