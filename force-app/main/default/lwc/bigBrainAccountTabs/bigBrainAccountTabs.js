a5fb383846f7515410675baebab47dd7c39b1854
eranbenmoha <eranb@monday.com>
2021-07-08 10:51:33 +0300

    Update bigBrainAccountTabs.js

--- force-app/main/default/lwc/bigBrainAccountTabs/bigBrainAccountTabs.js
+++ force-app/main/default/lwc/bigBrainAccountTabs/bigBrainAccountTabs.js
@@ -6,16 +6,20 @@ import OPP_ACCOUNT_FIELD from "@salesforce/schema/Opportunity.primary_pulse_acco
 const fields = [LEAD_ACCOUNT_FIELD, OPP_ACCOUNT_FIELD];
 
 export default class BigBrainAccountTabs extends LightningElement {
-    @track record; 
+    @api recordId;
     @track pulseAccountId;
 
-    @api recordId;
-    @wire(getRecord, { recordId: '$recordId', fields })
+    // Getting by lead
+    @wire(getRecord, { recordId: '$recordId', fields: [LEAD_ACCOUNT_FIELD] })
     wiredRecord({ error, data }) {
-        if (data) { 
-            const leadPulseAccountId = getFieldValue(data, LEAD_ACCOUNT_FIELD);
-            const opportunityPulseAccountId = getFieldValue(data, OPP_ACCOUNT_FIELD);
-            this.pulseAccountId = leadPulseAccountId || opportunityPulseAccountId;
-        }
+        const pulseAccountId = getFieldValue(data, LEAD_ACCOUNT_FIELD);
+        if (pulseAccountId) { this.pulseAccountId = pulseAccountId; }
+    }
+
+    // Getting by opportunity
+    @wire(getRecord, { recordId: '$recordId', fields: [OPP_ACCOUNT_FIELD] })
+    wiredRecord({ error, data }) {
+        const pulseAccountId = getFieldValue(data, OPP_ACCOUNT_FIELD);
+        if (pulseAccountId) { this.pulseAccountId = pulseAccountId; }
     }
 }
