223d5960bb738d6a1778ee27bc06c6a3eeb7bd7d
michaelimas <90142755+michaelimas@users.noreply.github.com>
2021-11-30 11:15:20 +0200

    added validation (#205)

--- force-app/main/default/lwc/quote_renewalCreation/quote_renewalCreation.js
+++ force-app/main/default/lwc/quote_renewalCreation/quote_renewalCreation.js
@@ -3,6 +3,7 @@ import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import createRenewalSalesOrder from '@salesforce/apex/Renewal_Flat_CreateSalesOrder.createRenewalSalesOrder';
 // import checkNewQuotes from '@salesforce/apex/Renewal_Flat_CreateSalesOrder.checkNewQuotes';
 import originalContract from '@salesforce/schema/Opportunity.Selected_Company_Contract__c';
+import paymentTerms from '@salesforce/schema/Opportunity.Payment_Terms__c';
 import billingEntity from '@salesforce/schema/Opportunity.Billing_Entity__c';
 import contractSeats from '@salesforce/schema/Opportunity.Selected_Company_Contract__r.Contract_Seats__c';
 import contractEndDate from '@salesforce/schema/Opportunity.Selected_Company_Contract__r.EndDate';
@@ -25,6 +26,7 @@ export default class Quote_renewalCreation extends LightningElement{
     showCreateButton=true;
     oppDetails;
     originalContract;
+    paymentTerms;
     billingEntity;
     contractSeats;
     contractSeats;
@@ -34,11 +36,12 @@ export default class Quote_renewalCreation extends LightningElement{
     contractUnitPrice;
     missingBe = false;
     missingContract = false;
+    missingPaymentTerms = false;
     loading=false;
     loadingModal=true;
     StartDate;
     total=0;
-    formFields= [originalContract,billingEntity,contractSeats,contractEndDate,contractTier,contractPeriod,contractUnitPrice]; 
+    formFields= [originalContract,billingEntity,contractSeats,paymentTerms,contractEndDate,contractTier,contractPeriod,contractUnitPrice]; 
 
     @wire(getRecord, { recordId: '$recordId', fields })
     opp({data, error}){
@@ -46,11 +49,9 @@ export default class Quote_renewalCreation extends LightningElement{
             this.oppDetails=data;
             this.originalContract= getFieldValue(this.oppDetails, originalContract);
             this.billingEntity=getFieldValue(this.oppDetails, billingEntity);
+            this.paymentTerms=getFieldValue(this.oppDetails, paymentTerms);
             this.contractSeats=getFieldValue(this.oppDetails, contractSeats);
             this.contractEndDate=getFieldValue(this.oppDetails, contractEndDate);
-            // var contractStartDate = new Date(this.contractEndDate);
-            // contractStartDate.setDate(contractStartDate.getDate() + 1);
-            // this.StartDate = contractStartDate;
             this.contractTier=getFieldValue(this.oppDetails, contractTier);
             this.contractPeriod=getFieldValue(this.oppDetails, contractPeriod);
             this.contractUnitPrice=getFieldValue(this.oppDetails, contractUnitPrice);
@@ -68,6 +69,9 @@ export default class Quote_renewalCreation extends LightningElement{
             if(this.billingEntity == null) {
                 this.missingBe = true;
             }
+            if(this.paymentTerms == null) {
+                this.missingPaymentTerms = true;
+            }
             this.loadingModal=false;
         } 
         else if (error) {
@@ -83,11 +87,11 @@ export default class Quote_renewalCreation extends LightningElement{
     }
     handleStart(e) {
         
-        if (this.missingContract || this.missingBe) {
+        if (this.missingContract || this.missingBe || this.missingPaymentTerms) {
             this.dispatchEvent(
                 new ShowToastEvent({
                     title: 'Required fields are missing!',
-                    message: 'Make sure to link a billing entity and a primary contract to opportunity before creating a renewal SO',
+                    message: 'Make sure to have a billing entity, primary contract and payment terms on the opportunity before creating a renewal SO',
                     variant: 'error',
                 }),
             );
