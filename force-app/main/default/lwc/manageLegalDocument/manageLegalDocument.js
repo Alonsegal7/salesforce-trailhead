26e29d59f0cc73e9c2dcea0f287c55d266b182ca
alonsegal77 <83406786+alonsegal77@users.noreply.github.com>
2022-02-14 09:35:43 +0200

    Clm enhenc alon (#280)
    * v.1
    
    * commit
    
    * change InitAddendum url
    
    * is sanbox check
    
    * InitAddendum
    
    * commit
    
    * url updates
    
    * prod url
    
    * url update
    
    * closeAction
    
    * link
    
    * isSandbox
    
    Co-authored-by: “alonsegal77” <“alonse@monday.com”>

--- force-app/main/default/lwc/manageLegalDocument/manageLegalDocument.js
+++ force-app/main/default/lwc/manageLegalDocument/manageLegalDocument.js
@@ -3,6 +3,7 @@ import { NavigationMixin, CurrentPageReference } from 'lightning/navigation';
 import { CloseActionScreenEvent } from 'lightning/actions';
 import getSalesOrderInfo from '@salesforce/apex/ManageLegalDocumentController.getSalesOrderDetails';
 import checkIfLegalDocExist from '@salesforce/apex/ManageLegalDocumentController.legalDocExistForCompany';
+import isSandbox from '@salesforce/apex/ManageLegalDocumentController.isSandbox';
 
 const companyLegalDocuments = [
     { label: 'Document type', fieldName: 'Document_type__c', type: 'text' },
@@ -19,6 +20,7 @@ export default class ManageLegalDocument extends NavigationMixin(LightningElemen
     missingRelevantInfo=false;
     showSaaSbutton=false;
     showDPAbutton=false;
+    showAddendum=false;
     salesOrderSelectedAgreementType;
     columns = companyLegalDocuments;
     showCompanyLegalDocTable=false;
@@ -27,6 +29,7 @@ export default class ManageLegalDocument extends NavigationMixin(LightningElemen
     oppName;
     oppCompanyId;
     oppCompanyName;
+    isSandbox=false;
 
     @wire(getSalesOrderInfo,{oppId:'$recordId'})
         getSalesOrderDetails({data, error}){
@@ -44,7 +47,7 @@ export default class ManageLegalDocument extends NavigationMixin(LightningElemen
                     this.salesOrderSelectedAgreementType='SaaS Agreement';
                 }
                 //We only support SaaS and TOS + DPA Agreements (For Now)
-                if (this.salesOrderSelectedAgreementType!='SaaS Agreement' && this.salesOrderSelectedAgreementType!='TOS + DPA') {
+                if (this.salesOrderSelectedAgreementType!='SaaS Agreement' && this.salesOrderSelectedAgreementType!='TOS + DPA'  && this.salesOrderSelectedAgreementType!='Addendum (negotiated)' ) {
                     this.missingRelevantInfo=true;
                 }
                 //Check if there is a clm process under this SO legal agreement type
@@ -81,6 +84,10 @@ checkIfLegalDocExist(){
             this.legalDocExistForCompany=false;
             this.showDPAbutton=true;
         }
+        else if (this.salesOrderSelectedAgreementType=='Addendum (negotiated)' ) {
+            this.legalDocExistForCompany=false;
+            this.showAddendum=true;
+        }
 
     }).catch(error => {
         console.log(error.body.message);
@@ -95,15 +102,44 @@ checkIfLegalDocExist(){
 }
 //Each clm doc url will have a different url and therefore a different buttons
     InitSaasAgreement(event){
+        this.closeAction();
+        if (isSandbox()) {
+        var thisUrl ="https://uatna11.springcm.com/atlas/doclauncher/eos/Monday SaaS Agreement?aid=25283&eos[0].Id="+this.recordId+"&eos[0].System=Salesforce&eos[0].Type=Opportunity&eos[0].Name="+this.oppName+"&eos[0].ScmPath=/Salesforce/Companies/"+this.oppCompanyName+"/Opportunities/";
+        window.open(thisUrl,'_blank');
+        }
+        else{
         var thisUrl ="https://na11.springcm.com/atlas/doclauncher/eos/Monday SaaS Agreement?aid=22364&eos[0].Id="+this.recordId+"&eos[0].System=Salesforce&eos[0].Type=Opportunity&eos[0].Name="+this.oppName+"&eos[0].ScmPath=/Salesforce/Companies/"+this.oppCompanyName+"/Opportunities/";
         window.open(thisUrl,'_blank');
+        }
     }
 
     InitDPA(event){
+        this.closeAction();
+        if (isSandbox()) {
+        var thisUrl ="https://uatna11.springcm.com/atlas/doclauncher/eos/DPA?aid=25283&eos[0].Id="+this.recordId+"&eos[0].System=Salesforce&eos[0].Type=Opportunity&eos[0].Name="+this.oppName+"&eos[0].ScmPath=/Salesforce/Companies/"+this.oppCompanyName+"/Opportunities/";
+        window.open(thisUrl,'_blank');
+        }
+        else{
         var thisUrl ="https://na11.springcm.com/atlas/doclauncher/eos/DPA?aid=22364&eos[0].Id="+this.recordId+"&eos[0].System=Salesforce&eos[0].Type=Opportunity&eos[0].Name="+this.oppName+"&eos[0].ScmPath=/Salesforce/Companies/"+this.oppCompanyName+"/Opportunities/";
         window.open(thisUrl,'_blank');
+        }
+    } 
+
+    InitAddendum(event){
+        this.closeAction();
+        if (isSandbox()) {
+            var thisUrl ="https://uatna11.springcm.com/atlas/Forms/UpdateFormDoc.aspx?aid=25283&FormUid=43589d9d-556d-ec11-b821-48df378a7098&oppId="+this.recordId;
+            window.open(thisUrl,'_blank');
+        }
+        else{
+            var thisUrl ="https://na11.springcm.com/atlas/Forms/UpdateFormDoc.aspx?aid=22364&FormUid=b4feae6a-e772-ec11-9c46-d89d6716196d&oppId="+this.recordId;
+            window.open(thisUrl,'_blank');
+        }
+        {!Opportunity.Id}
+
     }        
     closeAction(){
         this.dispatchEvent(new CloseActionScreenEvent());
     }
 }
+
