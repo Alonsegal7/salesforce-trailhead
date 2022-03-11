5dc2236cb8923f045aca98c38745482859db6a42
kseniasma <82751407+kseniasma@users.noreply.github.com>
2022-03-11 18:28:06 +0200

    co-sell 2.0 pilot - survey community display (#322)
    * temp
    
    * debug

--- force-app/main/default/lwc/opportunity_PartnerImpactSurveyCosell/opportunity_PartnerImpactSurveyCosell.js
+++ force-app/main/default/lwc/opportunity_PartnerImpactSurveyCosell/opportunity_PartnerImpactSurveyCosell.js
@@ -1,30 +1,50 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord, getFieldValue, updateRecord } from 'lightning/uiRecordApi';
 import Id from '@salesforce/user/Id';
-import COSELL_REQUEST_FIELD from '@salesforce/schema/Opportunity.Co_Sell_Request__c';
+import COSELL_REQUEST_ID from '@salesforce/schema/Opportunity.Co_Sell_Request__c';
+import COSELL_REQUEST_SURVEY_FILLED from '@salesforce/schema/Opportunity.Co_Sell_Request__r.Impact_Survey_Filled__c';
+import ACCOUNT_COSELL_LEADER from '@salesforce/schema/Opportunity.Account.Co_Sell_Leader__c';
+import OPP_RECORD_TYPE from '@salesforce/schema/Opportunity.RecordType.DeveloperName';
 
 export default class Opportunity_PartnerImpactSurveyCosell extends LightningElement {
     @api recordId;
-    targetId;
+    coSellReqId;
     load = false;
     userId = Id;
 
-    @wire(getRecord, { recordId: '$recordId', fields: [COSELL_REQUEST_FIELD]})
+    @wire(getRecord, { recordId: '$recordId', fields: [COSELL_REQUEST_ID, COSELL_REQUEST_SURVEY_FILLED, ACCOUNT_COSELL_LEADER, OPP_RECORD_TYPE]})
     wiredRecord({ error, data }) {
         if (data) {
-            this.targetId = getFieldValue(data, COSELL_REQUEST_FIELD);
-            this.load = true;
+            console.log('loading co-sell impact survey...');
+            this.coSellReqId = getFieldValue(data, COSELL_REQUEST_ID);
+            var recordType = getFieldValue(data, OPP_RECORD_TYPE);
+            var surveyFilled = getFieldValue(data, COSELL_REQUEST_SURVEY_FILLED);
+            var accountCosellLeader = getFieldValue(data, ACCOUNT_COSELL_LEADER);
+            console.log('co-sell request id: '+ this.coSellReqId);
+            console.log('opp record type: '+ recordType);
+            console.log('survey filled: '+ surveyFilled); //PROBLEM - RETURNS NULL B/C SHARING RULES
+            console.log('account Cosell Leader: '+ accountCosellLeader);
+            if(this.coSellReqId != null && this.coSellReqId != undefined 
+                && surveyFilled == false
+                && ((recordType == 'Partner_Opportunity' && accountCosellLeader == 'Partners')
+                    || (recordType == 'Internal_Opportunity' && accountCosellLeader == 'Sales'))){
+                this.load = true; //load survey
+                console.log('loading co-sell impact survey now!');
+            } else {
+                this.load = false; //do not load survey
+                console.log('NO co-sell impact survey needed!');
+            }
             this.error = undefined;
         } else if (error) {
             this.error = error;
-            this.targetId = undefined;
+            this.coSellReqId = undefined;
         }
     }
 
     handleSurveyFilledEvent(){
         // Create the recordInput object
         const fields = {};
-        fields.Id = this.targetId;
+        fields.Id = this.coSellReqId;
         fields.Impact_Survey_Responded_By__c = this.userId;
         fields.Impact_Survey_Responded_Date__c = this.getTodayDate();
 
