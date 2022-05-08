ff3cadce9aa9f0d2a007dd9087dd273dc9b00bbb
noamlevm <noamle@monday.com>
2022-05-08 16:08:21 +0300

    Updated with month handling

--- force-app/main/default/lwc/calcEndDate/calcEndDate.js
+++ force-app/main/default/lwc/calcEndDate/calcEndDate.js
@@ -4,35 +4,28 @@ export default class calcEndDate extends LightningElement {
     @api Duration; 
     @api StartDate;
     @api EndDate; 
-    @track StartDate;
-    @track Duration;
-    @track EndDate;
+    @api required;
 
     addDuration(dt, dur){
         var date = new Date(dt);
         date.setMonth(parseInt(date.getMonth()) + parseInt(dur));
-        console.log('addDuration post set: '+ date);
         return date;
     }
     deductDays(dt,days){
         var date = new Date(dt);
         date.setDate(date.getDate() - days);
-        console.log('Date after change '+ date);
         return date;
     }
     calcExpr() {
         try{
             var tempDate = new Date();
             tempDate = this.addDuration(this.StartDate, this.Duration);
-            console.log('tempDate addDuration: '+tempDate);
             tempDate = this.deductDays(tempDate , 1);
-            console.log('tempDate deductDays: '+tempDate);
             this.EndDate = new Date(tempDate);
             var DD = tempDate.getDate();
             var MM = tempDate.getMonth()+1;
             var YYYY = tempDate.getFullYear();
             this.EndDate = YYYY + '-' + MM + '-' + DD;
-            console.log('End date final '+this.EndDate);
         } catch(e){
             console.error(e);
             console.error('e.name => ' + e.name );
@@ -43,16 +36,28 @@ export default class calcEndDate extends LightningElement {
     handleChangeDate(evt) {
         this.StartDate = evt.target.value;
         if (this.StartDate != null && this.Duration != null){
-            this.calcExpr();
-        if (this.StartDate =null && this.Duration !=null){
-            this.EndDate=''
-        }
-        }
+            this.calcExpr();}
     }
     handleChangeDuration(evt) { 
         this.Duration = evt.target.value;
         if (this.StartDate != null && this.Duration != null){
-            this.calcExpr();
+            this.calcExpr();}
+    }
+    @api
+    validate(){ //validation 
+
+        if(this.required == true || this.required == 'true'){ // fileds are mandatory
+            if (this.Duration == null || this.StartDate== null){
+                var errorMessage = 'Start Date and Duration are mandatory fields';
+                return { 
+                    isValid: false,
+                    errorMessage: errorMessage
+            };
+            } else { // passed validation
+                return { isValid: true };
+            }
+        } else { // fields not mandatory - no validation
+            return { isValid: true };
         }
     }
 }
