0a3a6eacb90c4eb573a8bc1462fea38ac58f00db
noamlevm <noamle@monday.com>
2022-05-24 14:59:36 +0300

    With opp logic

--- force-app/main/default/lwc/redirectToURLButton/redirectToURLButton.js
+++ force-app/main/default/lwc/redirectToURLButton/redirectToURLButton.js
@@ -1,13 +1,19 @@
-import { LightningElement,api } from 'lwc';
+import {LightningElement,api} from 'lwc';
+import createServiceOpportunities from '@salesforce/apex/ServiceOpportunityCreation.createServiceOpportunities';
 
 export default class RedirectToURLButton extends LightningElement {    
-    @api record='';
-    goToDealHub(event)
-    {
-        console.log('record id: '+ record);
-        var thisUrl ="https://"+window.location.hostname.replace('lightning.force', 'my.salesforce')+"/apex/valt__beforeCanvasPrep?Id="+record+"&param=CreateQuoteFromOpp";
-        console.log('Raz thisUrl: '+thisUrl);
-        window.open(thisUrl,'_blank');
-    }
+    //@api record;
+    // handleClick(){
+    //     createServiceOpportunities({accountsforserviceIds:[this.record]}).then((response)=>{this.opportunityId = response[0].Id;
+    //     console.log('record id: '+ this.record);
+    //     var thisUrl ="https://"+window.location.hostname.replace('lightning.force', 'my.salesforce')+"/apex/valt__beforeCanvasPrep?Id="+this.opportunityId +"&param=CreateQuoteFromOpp";
+    //     console.log('Raz thisUrl: '+thisUrl);
+    //     window.open(thisUrl,'_blank');})                                                                               
+    // }
 }
 
+
+//this.closeAction();
+    // closeAction(){
+    //      this.dispatchEvent(new CloseActionScreenEvent());
+   //  }
